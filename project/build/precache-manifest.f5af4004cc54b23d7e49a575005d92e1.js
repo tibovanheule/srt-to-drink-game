846a0289a94e241aeaa63a62a51f28ed5534cdff
Tibo Vanheule <tibo@tibovanheule.space>
2020-03-10 17:24:08 +0100

    workings

---
+++ project/build/precache-manifest.f5af4004cc54b23d7e49a575005d92e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c956ceeb49f5aa1cf1f732936dfa08",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9c38260cb078593d42f2",
+    "url": "./static/js/2.ad5321b5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.ad5321b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a9520543848a073cd30",
+    "url": "./static/js/main.ad6ba2df.chunk.js"
+  },
+  {
+    "revision": "e8707a7e1e1a0232d853",
+    "url": "./static/js/runtime-main.650ebd97.js"
+  }
+]);
---
+++ project/src/App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import parser from 'subtitles-parser';
+
+export default class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.srt = this.srt.bind(this);
+    }
+
+
+    srt(event) {
+        if (event.target.files[0]!== undefined) {
+            const reader = new FileReader();
+            reader.onload = async (e) => {
+                const text = (e.target.result);
+                let result = parser.fromSrt(text, false);
+                let count = {};
+                result.forEach((e) => {
+                    let sentence = e.text.replace("'s", '').replace(/[!.?\-\*\$"']+/gi, '').replace('<i>', '').replace('</i>', '').toLowerCase().split(/[\s,\n]+/);
+                    sentence.forEach((e) => {
+                        if (count.hasOwnProperty(e)) count[e] += 1;
+                        else count[e] = 1;
+                    })
+                });
+                // Create items array
+                let countarray = Object.keys(count).map((key) => [key, count[key]]);
+                // Sort the array based on the second element
+                countarray.sort((first, second) => second[1] - first[1]);
+                this.setState({count: countarray});
+            };
+            reader.readAsText(event.target.files[0]);
+        }
+    }
+
+    state = {count: undefined};
+
+    render() {
+        return (
+            <div>
+                <p>Upload your srt file</p>
+                <input type={"file"} onChange={this.srt}/>
+                {this.state.count !== undefined && <div id={"result"}>
+                    {this.state.count.map((key) =>
+                        <p>{key[0] + ": " + key[1]}</p>
+                    )}
+                </div>}
+            </div>
+        );
+    }
+
+
+}
